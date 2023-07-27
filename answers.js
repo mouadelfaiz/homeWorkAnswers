daa563e41be7e8d68fdf8ee7d2d4b03e5e5ad15e
mouadelfaiz <44578535+mouadelfaiz@users.noreply.github.com>
2023-07-27 13:59:14 -0400

    Yell at the Ninja Turtles answered

--- answers.js
+++ answers.js
@@ -15,45 +15,83 @@
 //Easy Going
 ///////////////////////////
 
-for (let i = 1; i <= 20; i++) {
-    console.log(i)
-}
+// for (let i = 1; i <= 20; i++) {
+//     console.log(i)
+// }
 
 ///////////////////////////
 //Get Even
 ////////////////////////
 
-for (let i = 0 ; i <= 200; i++) {
-    if (i % 2 === 0) {
-        console.log(i)
-    }
-}
+// for (let i = 0 ; i <= 200; i++) {
+//     if (i % 2 === 0) {
+//         console.log(i)
+//     }
+// }
 
 ///////////////////////////////////
 //Fizz Buzz
 ////////////////////////////
 
+// for (let i = 1; i <= 100; i++) {
+//     if (i % 3 === 0) {
+//         if ( i % 5 === 0) {
+//             console.log("FizzBuzz")
+//         }if (i % 5 === 0) {
+//             console.log("Buzz")
+//         }
+//     }if (i % 3 === 0) {
+//         console.log("Fizz")
+//     }
+// }
 
 
 /////////////////////////////////
 //Wild Wild Life
 ///////////////////////////////
 
+// const wolfy = ["Wolfy", "wolf", 16, "Yukon Territory"]
+// const sharky = ["Sharky", "shark", 20, "Left Coast"]
+// const plantee = ["Plantee", "plant",  5000 , "Mordor"]
+// const porgee = ["Porgee", "Porg", 186, "Ahch-To"]
+// const dart = ["D'Art" , "Demogorgan Dog", 2, "Upside Down"]
+
+// plantee[2] += 1;
+// wolfy[3] = "Gotham City";
+// dart.push("Hawkins");
+// wolfy[0] = ("Gameboy")
+// console.log(plantee)
+// console.log(wolfy)
+// console.log(dart)
+// console.log(wolfy)
 //////////////////////////////////////////
 //Yell at the Ninja Turtles
 ///////////////////////////////////
 
+// const ninja = ["Donatello", "Leonardo", "Raphael", "Michaelangelo"]
+
+// for (const ninjas of ninja) {
+//     console.log(ninjas.toUpperCase())
+// }
 
 ////////////////////////////////////////////////
 //Methods, Revisited
 ///////////////////////////////////////////////
 
+const favMovies = ['Jaws', 'The Fellowship of the Ring', 'Howl\'s Moving Castle', 'Django Unchained', 'Cloud Atlas', 'The Usual Suspects', 'Toy Story', 'Conan the Barbarian', 'Titanic', 'Harry Potter', 'Fried Green Tomatoes', 'Volver', 'Oculus', 'Seven', 'Black Panther', 'Harry Potter', 'Imitation of Life', 'Snatch', 'Fast and Furious'];
+
+favMovies.push('Fight Club')
+favMovies.push('Interstaller')
+console.log(favMovies.indexOf('Titanic'));
+console.log(favMovies)
+//1
 
 
 
 //////////////////////////////////////////////////
 //Where is Waldo
 //////////////////////////////////////////////////////////
+
 
 
 
